f760b5f79ffdf3a09007ce2dc309987f80bdb085
victormatia <fvictor1705@gmail.com>
2024-11-24 23:48:02 -0300

    backend:refactor: remove orm GenericModels property

--- backend/src/models/GenericModel.ts
+++ backend/src/models/GenericModel.ts
@@ -1,12 +1,10 @@
-import { PrismaClient } from '@prisma/client';
 import IMutation from '../interfaces/IMutation';
 import IQuery from '../interfaces/IQuery';
 
 abstract class GenericModel<T> implements IQuery<T>, IMutation<T> {
-  private _orm: PrismaClient;
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public async create(data: unknown): Promise<T> {
+  public async create(data: T): Promise<T> {
     return {} as T;
   }
 
