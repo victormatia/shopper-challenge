8acf8e090ac335a2aebecea943c7f68127b2abce
victormatia <fvictor1705@gmail.com>
2024-11-26 13:05:27 -0300

    backend:refactor: adds status code logic on BadRequestError class

--- backend/src/Error/BadRequestError.ts
+++ backend/src/Error/BadRequestError.ts
@@ -1,8 +1,9 @@
+import { ErrorCodeEnum } from '../types/ErrorCodeEnum';
 import ApiError from './ApiError';
 
 class BadRequestError extends ApiError {
   constructor(message: string) {
-    super(message, 400);
+    super(message, 400, ErrorCodeEnum.INVALID_DATA);
   }
 }
 
