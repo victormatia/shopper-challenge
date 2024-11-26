77de223f7fef2ddecaf152575c8b8a12829b44e5
victormatia <fvictor1705@gmail.com>
2024-11-26 13:23:32 -0300

    backend:feat: creates ride routes file

---
+++ backend/src/routes/ride.routes.ts
@@ -0,0 +1,15 @@
+import RideService from '../services/RideService';
+import RideController from '../controllers/RideController';
+import { Router } from 'express';
+import RideModel from '../models/RideModel';
+import prisma from '../libs/PrismaClient';
+
+const router = Router();
+
+const model = new RideModel(prisma);
+const service = new RideService(model);
+const controller = new RideController(service);
+
+router.post('/estimate', controller.estimate.bind(controller));
+
+export default router;
