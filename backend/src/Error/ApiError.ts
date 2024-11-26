f7f1f21473d72c467ae4efdc6ca40cb4c5c3c758
victormatia <fvictor1705@gmail.com>
2024-11-26 13:01:27 -0300

    backend:feat:  adds status_code property on ApiError class

--- backend/src/Error/ApiError.ts
+++ backend/src/Error/ApiError.ts
@@ -1,11 +1,18 @@
+import { ErrorCodeEnum } from '../types/ErrorCodeEnum';
+
 class ApiError extends Error {
-  constructor(message: string, private _status: number) {
+  constructor(message: string, private _status: number, private _status_code: ErrorCodeEnum) {
     super(message);
   }
 
   public get status(): number {
     return this._status;
   }
+
+  public get status_code(): string {
+    return ErrorCodeEnum[this._status];
+  }
+
 }
 
 export default ApiError;
