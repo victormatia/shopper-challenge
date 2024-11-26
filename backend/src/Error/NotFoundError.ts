09945c7872e247306243abb7c5f5f47c9b44159b
victormatia <fvictor1705@gmail.com>
2024-11-26 13:04:05 -0300

    backend:refactor: adds status code logic in InternalServerError, NotAuthorizedError and NotFountError classes

--- backend/src/Error/NotFoundError.ts
+++ backend/src/Error/NotFoundError.ts
@@ -1,8 +1,9 @@
+import { ErrorCodeEnum } from '../types/ErrorCodeEnum';
 import ApiError from './ApiError';
 
 class NotFoundError extends ApiError {
   constructor(message: string) {
-    super(message, 404);
+    super(message, 404, ErrorCodeEnum.DRIVER_NOT_FOUND);
   }
 }
 
